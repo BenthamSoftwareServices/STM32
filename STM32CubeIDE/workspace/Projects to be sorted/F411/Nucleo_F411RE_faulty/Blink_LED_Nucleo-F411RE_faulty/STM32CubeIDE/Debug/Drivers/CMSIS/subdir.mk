46da978237bd159a8d56b1d7ef9f1bc320fd153d
Steven Bennett <benthamsoftwareservices@gmail.com>
2025-04-22 16:12:36 +0100

    Updates

---
+++ STM32CubeIDE/workspace/Projects to be sorted/F411/Nucleo_F411RE_faulty/Blink_LED_Nucleo-F411RE_faulty/STM32CubeIDE/Debug/Drivers/CMSIS/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+E:/STM32-Projects/Blink_LED_Nucleo-F411RE/Src/system_stm32f4xx.c 
+
+OBJS += \
+./Drivers/CMSIS/system_stm32f4xx.o 
+
+C_DEPS += \
+./Drivers/CMSIS/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/CMSIS/system_stm32f4xx.o: E:/STM32-Projects/Blink_LED_Nucleo-F411RE/Src/system_stm32f4xx.c Drivers/CMSIS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../../Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/CMSIS/system_stm32f4xx.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
