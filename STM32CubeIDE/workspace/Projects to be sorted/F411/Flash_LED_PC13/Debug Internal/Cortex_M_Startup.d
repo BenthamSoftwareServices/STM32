28bbe7d5f8f6a73fb91356f9ea41d369ea277f4b
Steven Bennett <benthamsoftwareservices@gmail.com>
2025-04-22 15:21:30 +0100

    Updates

---
+++ STM32CubeIDE/workspace/Projects to be sorted/F411/Flash_LED_PC13/Debug Internal/Cortex_M_Startup.d
@@ -0,0 +1,2 @@
+Debug\ Internal/Cortex_M_Startup.o: \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.44/samples/Cortex_M_Startup.s
---
+++ STM32CubeIDE/workspace/Projects to be sorted/F411/Flash_LED_PC13/Debug Internal/sysmem.d
@@ -0,0 +1,8 @@
+Debug Internal/sysmem.o: \
+  E:\Cloud\ Storage\Dropbox\Personal\Electronics\MCUs\ARM\STM\ Microelectronics\Projects\Flash_LED_PC13\Src\sysmem.c \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 5.44\segger-rtl\include\errno.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 5.44\segger-rtl\include\__SEGGER_RTL.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 5.44\segger-rtl\include\__SEGGER_RTL_ConfDefaults.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 5.44\segger-rtl\include\__SEGGER_RTL_Conf.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 5.44\segger-rtl\include\__SEGGER_RTL_Arm_Conf.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 5.44\segger-rtl\include\stdint.h
