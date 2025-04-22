cd8f9b8be40f1dbe891b9fcbdaa003cc0e7cc25c
Steven Bennett <benthamsoftwareservices@gmail.com>
2025-04-22 16:35:15 +0100

    Updates

--- STM32CubeIDE/workspace/LCD_1602_I2C_Display_with_Nucleo_STM32L432KC/Core/Src/main.c
+++ STM32CubeIDE/workspace/LCD_1602_I2C_Display_with_Nucleo_STM32L432KC/Core/Src/main.c
@@ -95,58 +95,59 @@ int main(void) {
 	MX_USART2_UART_Init();
 	MX_I2C1_Init();
 	/* USER CODE BEGIN 2 */
-	HD44780_Init(2);
-	HD44780_Clear();
-	HD44780_SetCursor(0, 0);
-	HD44780_PrintStr("HELLO");
-	HD44780_SetCursor(10, 1);
-	HD44780_PrintStr("WORLD");
-	HAL_Delay(2000);
 
-	HD44780_Clear();
-	HD44780_SetCursor(0, 0);
-	HD44780_PrintStr("HELLO");
-	HAL_Delay(2000);
-	HD44780_NoBacklight();
-	HAL_Delay(2000);
-	HD44780_Backlight();
-
-	HAL_Delay(2000);
-	HD44780_Cursor();
-	HAL_Delay(2000);
-	HD44780_Blink();
-	HAL_Delay(5000);
-	HD44780_NoBlink();
-	HAL_Delay(2000);
-	HD44780_NoCursor();
-	HAL_Delay(2000);
-
-	HD44780_NoDisplay();
-	HAL_Delay(2000);
-	HD44780_Display();
-
-	HD44780_Clear();
-	HD44780_SetCursor(0, 0);
-	HD44780_PrintStr("Learning STM32 with LCD is fun :-)");
-
-	for (int x = 0; x < 40; x = x + 1) {
-		HD44780_ScrollDisplayLeft();  //HD44780_ScrollDisplayRight();
-		HAL_Delay(500);
-	}
-
-	char snum[5];
-	for (int x = 1; x <= 200; x++) {
-		snprintf(snum, sizeof(snum), "%d", x);
-		HD44780_Clear();
-		HD44780_SetCursor(0, 0);
-		HD44780_PrintStr(snum);
-		HAL_Delay(1000);
-	}
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
 	while (1) {
+		HD44780_Init(2);
+		HD44780_Clear();
+		HD44780_SetCursor(0, 0);
+		HD44780_PrintStr("HELLO");
+		HD44780_SetCursor(10, 1);
+		HD44780_PrintStr("WORLD");
+		HAL_Delay(2000);
+
+		HD44780_Clear();
+		HD44780_SetCursor(0, 0);
+		HD44780_PrintStr("HELLO");
+		HAL_Delay(2000);
+		HD44780_NoBacklight();
+		HAL_Delay(2000);
+		HD44780_Backlight();
+
+		HAL_Delay(2000);
+		HD44780_Cursor();
+		HAL_Delay(2000);
+		HD44780_Blink();
+		HAL_Delay(5000);
+		HD44780_NoBlink();
+		HAL_Delay(2000);
+		HD44780_NoCursor();
+		HAL_Delay(2000);
+
+		HD44780_NoDisplay();
+		HAL_Delay(2000);
+		HD44780_Display();
+
+		HD44780_Clear();
+		HD44780_SetCursor(0, 0);
+		HD44780_PrintStr("Learning STM32 with LCD is fun :-)");
+
+		for (int x = 0; x < 40; x = x + 1) {
+			HD44780_ScrollDisplayLeft();  //HD44780_ScrollDisplayRight();
+			HAL_Delay(500);
+		}
+
+		char snum[5];
+		for (int x = 1; x <= 200; x++) {
+			snprintf(snum, sizeof(snum), "%d", x);
+			HD44780_Clear();
+			HD44780_SetCursor(0, 0);
+			HD44780_PrintStr(snum);
+			HAL_Delay(1000);
+		}
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
