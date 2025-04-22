2bd72f6b70b1e75f34a8920728342d029b66a7cb
Steven Bennett <benthamsoftwareservices@gmail.com>
2025-04-22 15:23:40 +0100

    Updates

--- STM32CubeIDE/workspace/Projects to be sorted/Flash_LED_PC13/Debug Internal/Cortex_M_Startup.d
+++
@@ -1,2 +0,0 @@
-Debug\ Internal/Cortex_M_Startup.o: \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.44/samples/Cortex_M_Startup.s
--- STM32CubeIDE/workspace/Projects to be sorted/Flash_LED_PC13/Debug Internal/SEGGER_THUMB_Startup.d
+++
@@ -1,2 +0,0 @@
-Debug\ Internal/SEGGER_THUMB_Startup.o: \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.44/samples/SEGGER_THUMB_Startup.s
